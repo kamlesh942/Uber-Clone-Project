de1cd13e233d6c0d9f4b977ccd72d10e62ed523c
kamlesh rajak <kamleshrajak888@gmail.com>
2025-11-20 12:42:07 +0530

    Add Home page layout with background and navigation link

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
+
+
   return (
-    <div>Home</div>
+    <div>
+      <div className='bg-[url(https://images.unsplash.com/photo-1518430272387-254558840136?q=80&w=1063&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-cover h-screen pt-9 flex flex-col justify-between w-full bg-red-400'>
+        {/* <img className='w-16 ml-8' src="https://download.logo.wine/logo/Uber/Uber-Logo.wine.png" alt="" /> */}
+        <h2 className='w-16 ml-8 text-white font-bold text-xl'>Uber</h2>
+        <div className='bg-white pb-7 py-4 px-4'>
+          <h2 className='text-2xl font-bold'>Get Started with Uber</h2>
+          <Link to="/user-login" className='w-full bg-black text-white py-3 rounded mt-4 font-semibold flex items-center justify-center'>Continue</Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
