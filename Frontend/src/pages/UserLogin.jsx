10a6e045b891628131308012e4b9c3247d830797
kamlesh rajak <kamleshrajak888@gmail.com>
2025-11-22 23:00:27 +0530

    added submit handler

--- Frontend/src/pages/UserLogin.jsx
+++ Frontend/src/pages/UserLogin.jsx
@@ -1,16 +1,37 @@
-import React from 'react'
+import React, { use, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const UserLogin = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log( email, password);
+    setEmail('');
+    setPassword('');
+  }
   return (
     <div className='p-7 flex flex-col justify-between h-screen'>
       <div>
         <img className='w-16 mb-7' src="https://download.logo.wine/logo/Uber/Uber-Logo.wine.png" alt="" />
-      <form>
+      <form onSubmit={(e)=>{
+        
+        submitHandler(e)
+        
+        }}
+        >
+
         <h3 className='text-lg mb-2'>What's your email</h3>
-        <input className='bg-[#eee] mb-7 outline-none rounded px-4 py-2 border w-full text-lg placeholder:text-lg' type="email" placeholder='example@gmail.com' required/>
+        <input 
+        value={email}
+        onChange= {(e)=> setEmail(e.target.value)}
+        className='bg-[#eee] mb-7 outline-none rounded px-4 py-2 border w-full text-lg placeholder:text-lg' type="email" placeholder='example@gmail.com' required/>
         <h3 className='text-lg mb-2'>Password</h3>
-        <input className='bg-[#eee] mb-7 outline-none rounded px-4 py-2 border w-full text-lg placeholder:text-lg' type="password" placeholder='Enter your password' required/>
+        <input 
+        value={password}
+        onChange={(e)=>setPassword(e.target.value)}
+        className='bg-[#eee] mb-7 outline-none rounded px-4 py-2 border w-full text-lg placeholder:text-lg' type="password" placeholder='Enter your password' required/>
         <button className='bg-[#212121] text-white mb-3 py-2 px-5 font-semibold rounded-lg w-full' type="submit">Login</button>
         
       </form>
