78fdbcd88927e46e4efe5a2a8c4005762a2ee51b
kamlesh rajak <kamleshrajak888@gmail.com>
2025-12-06 23:37:42 +0530

    Started making User Profile page

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='w-screen h-screen relative'>
+     
+      <img className='w-20 absolute left-5 top-5' src="https://brandeps.com/logo-download/U/Uber-logo-02.png" alt="Uber-Logo" />
+      <div className="h-screen w-screen">
+        {/* for temporary purpose */}
+        <img className='h-full w-full' src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="Uber-map-img" />
+      </div>
+      <div className='bg-white absolute top-0 w-full p-5'>
+        <h4 className='text-3xl font-semibold'>Find a Trip</h4>
+        <form>
+          <input className='bg-[#eee] px-8 py-2 text-lg rounded-lg mt-5' type="text" placeholder='Add a Pick-up Location' />
+          <input className='bg-[#eee] px-8 py-2 text-lg rounded-lg mt-5' type="text" placeholder='Enter Your Destinatio' />
+        </form>
+      </div>
+    </div>
   )
 }
 
--- Frontend/src/pages/UserProtectWrappers.jsx
+++ Frontend/src/pages/UserProtectWrappers.jsx
@@ -1,6 +1,8 @@
-import React, {useContext, useEffect} from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import { useNavigate } from 'react-router-dom'
 import { UserContext } from "../context/userContext";
+import axios from "axios";
+
 
 
 
@@ -11,11 +13,31 @@ const UserProtectWrappers = ({
     const token = localStorage.getItem("token");
     const navigate = useNavigate();
     const {user, setUser} = useContext(UserContext);
+    const [isLoading, setIsLoading] = useState(true);
     useEffect(() => {
         if(!token){
             navigate('/user-login');
         }
+        axios.get(`${import.meta.env.VITE_BASE_URL}/users/profile`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+        }).then((response) => {
+
+            if(response.status === 200){
+                setUser(response.data.user);
+                setIsLoading(false);
+            }
+        }).catch((error) => {
+            console.error("Error fetching user profile:", error);
+            localStorage.removeItem("token");
+            navigate('/user-login');
+        })
     }, [token]);
+
+    if(isLoading){
+        return <div>Loading...</div>;
+    }
   return (
     <>
         {children}
