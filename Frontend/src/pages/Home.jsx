09b54e711e6e6420f7a7a88b29b8aff86bae4ab4
kamlesh rajak <kamleshrajak888@gmail.com>
2025-12-07 23:48:40 +0530

    continued home user profile page

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,23 +1,83 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
 
 const Home = () => {
+  const [pickup, setPickup] = useState("");
+  const [destination, setDestination] = useState("");
+  const [panelOpen, setpanelOpen] = useState(false);
+  const panelRef = useRef(null);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("form submitted");
+  };
+  useGSAP(() => {
+    if (panelOpen) {
+      gsap.to(panelRef.current, {
+        height: "70%",
+      });
+    } else {
+      gsap.to(panelRef.current, {
+        height: "0",
+      });
+    }
+  },[panelOpen]);
   return (
-    <div className='w-screen h-screen relative'>
-     
-      <img className='w-20 absolute left-5 top-5' src="https://brandeps.com/logo-download/U/Uber-logo-02.png" alt="Uber-Logo" />
+    <div className="w-screen h-screen relative">
+      <img
+        className="w-20 absolute left-5 top-5"
+        src="https://brandeps.com/logo-download/U/Uber-logo-02.png"
+        alt="Uber-Logo"
+      />
       <div className="h-screen w-screen">
         {/* for temporary purpose */}
-        <img className='h-full w-full' src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="Uber-map-img" />
+        <img
+          className="h-full w-full"
+          src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif"
+          alt="Uber-map-img"
+        />
       </div>
-      <div className='bg-white absolute top-0 w-full p-5'>
-        <h4 className='text-3xl font-semibold'>Find a Trip</h4>
-        <form>
-          <input className='bg-[#eee] px-8 py-2 text-lg rounded-lg mt-5' type="text" placeholder='Add a Pick-up Location' />
-          <input className='bg-[#eee] px-8 py-2 text-lg rounded-lg mt-5' type="text" placeholder='Enter Your Destinatio' />
-        </form>
+      <div className="flex flex-col justify-end h-screen absolute top-0 w-full">
+        <div className="h-[30%] bg-white relative">
+          <h4 className="text-2xl font-semibold">Find a Trip</h4>
+          <form
+            onSubmit={(e) => {
+              submitHandler(e);
+            }}
+          >
+            <div className="line absolute h-16 w-1 top-[45%] left-10 bg-gray-900 rounded-full"></div>
+            <input
+              onClick={() => {
+                setpanelOpen(true);
+              }}
+              value={pickup}
+              onChange={(e) => {
+                setPickup(e.target.value);
+              }}
+              className="bg-[#eee] px-12 py-2 text-lg rounded-lg w-full mt-5"
+              type="text"
+              placeholder="Add a Pick-up Location"
+            />
+            <input
+              onClick={() => {
+                setpanelOpen(true);
+              }}
+              value={destination}
+              onChange={(e) => {
+                setDestination(e.target.value);
+              }}
+              className="bg-[#eee] px-12 py-2 text-lg rounded-lg w-full mt-5"
+              type="text"
+              placeholder="Enter Your Destinatio"
+            />
+          </form>
+        </div>
+        <div ref={panelRef}
+        className="h-0 bg-red-500"></div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
