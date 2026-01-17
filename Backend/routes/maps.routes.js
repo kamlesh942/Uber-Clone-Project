1517b6e64a8eefe363d6aa15d4b47da45f0ef964
kamlesh rajak <kamleshrajak888@gmail.com>
2026-01-17 22:11:16 +0530

    fixed a bug in maps Route

--- Backend/routes/maps.routes.js
+++ Backend/routes/maps.routes.js
@@ -4,5 +4,11 @@ const authMiddleware = require('../middlewares/auth.middlewares');
 const mapController = require('../controllers/maps.controller');
 const { query } = require('express-validator');
 
-router.get('/get-coordinates', query('address').isString().isLength({min: 3}),
-        authMiddleware.authUser, mapController.getCoordinates);
+router.get(
+  '/get-coordinates',
+  query('address').isString().isLength({ min: 3 }),
+  authMiddleware.authUser,
+  mapController.getCoordinates
+);
+
+module.exports = router;
