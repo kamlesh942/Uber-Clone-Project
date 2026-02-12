ef1a93cb7b49286c1f8088c18b20de0da3cb7739
kamlesh rajak <kamleshrajak888@gmail.com>
2026-02-12 12:24:35 +0530

    Update LocationSearchPanel to use suggestions prop and handle selection

--- Frontend/src/components/LocationSearchPanel.jsx
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -1,22 +1,28 @@
 import React from "react";
 import { FaLocationDot } from "react-icons/fa6";
+import { setMaxListeners } from "../../../Backend/models/ride.model";
+import { set } from "mongoose";
 
 
-const LocationSearchPanel = (props) => {
-  // sample array for location
-  const locations = [
-    "Vaibhav cafe second floor Kamlesh Coding Plateform Satna",
-    "Naman stores 1st floor Satna m.p",
-    "Agravaal chai suttabar stores 1st floor Satna m.p",
-    "Anmesh tayers stores 1st floor Satna m.p"
-  ]
+const LocationSearchPanel = ({suggetions, setVehiclePanel, setpanelOpen, setPickup, activeField, setDestination }) => {
+  const handleSuggetionsClick = (suggetion) =>{
+    if(activeField === "pickup"){
+      setPickup(suggetion);
+    }
+    else if(activeField === "destination"){
+      setDestination(suggetion);
+    }
+    // setVehiclePanel(true)
+    // setpanelOpen(false)
+  }
+
   return (
     <div>
     {
-      locations.map(function(elem, idx){
-        return <div key = {idx} onClick={()=>{
-          props.setVehiclePanel(true)
-          props.setpanelOpen(false)
+      suggetions.map(function(elem, idx){
+         <div key = {idx} onClick={()=>{
+          handleSuggetionsClick(elem)
+          
         }} className="flex gap-4  border-2 p-3 border-gray-50 active:border-black rounded-xl items-center my-4 justify-start">
         <h2 className=" bg-[#eee] flex items-center justify-center h-5 w-10 rounded-full">
              <FaLocationDot />
